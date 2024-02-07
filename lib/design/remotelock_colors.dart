1dccad48122158c046981c23dc972796aaab0eef
degnonepithech <josue.allagbe@epitech.eu>
2024-02-07 19:40:40 +0100

    Mise à jour'

--- lib/design/remotelock_colors.dart
+++ lib/design/remotelock_colors.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 @immutable
 class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
   const RemoteLockColors({
-    required this.toshoPurple,
-    required this.darkerToshoPurple,
+    required this.rtlockPurple,
+    required this.darkerRtlockPurple,
     required this.cadetGray,
     required this.antiFlashWhite,
     required this.systemRed,
@@ -13,13 +13,13 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
     required this.oxfordBlue,
     required this.white,
     required this.black,
-    required this.toshoBorderPurple,
+    required this.rtlockBorderPurple,
     required this.platinium,
     required this.gray85,
   });
 
-  final Color? toshoPurple;
-  final Color? darkerToshoPurple;
+  final Color? rtlockPurple;
+  final Color? darkerRtlockPurple;
   final Color? cadetGray;
   final Color? antiFlashWhite;
   final Color? systemRed;
@@ -28,14 +28,14 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
   final Color? oxfordBlue;
   final Color? white;
   final Color? black;
-  final Color? toshoBorderPurple;
+  final Color? rtlockBorderPurple;
   final Color? platinium;
   final Color? gray85;
 
   @override
   RemoteLockColors copyWith({
-    Color? toshoPurple,
-    Color? darkerToshoPurple,
+    Color? rtlockPurple,
+    Color? darkerrtlockPurple,
     Color? cadetGray,
     Color? antiFlashWhite,
     Color? systemRed,
@@ -44,13 +44,13 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
     Color? oxfordBlue,
     Color? white,
     Color? black,
-    Color? toshoBorderPurple,
+    Color? rtlockBorderPurple,
     Color? platinium,
     Color? gray85,
   }) {
     return RemoteLockColors(
-      toshoPurple: toshoPurple ?? this.toshoPurple,
-      darkerToshoPurple: darkerToshoPurple ?? this.darkerToshoPurple,
+      rtlockPurple: rtlockPurple ?? this.rtlockPurple,
+      darkerRtlockPurple: darkerrtlockPurple ?? this.darkerRtlockPurple,
       cadetGray: cadetGray ?? this.cadetGray,
       antiFlashWhite: antiFlashWhite ?? this.antiFlashWhite,
       systemRed: systemRed ?? this.systemRed,
@@ -59,7 +59,7 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
       oxfordBlue: oxfordBlue ?? this.oxfordBlue,
       white: white ?? this.white,
       black: black ?? this.black,
-      toshoBorderPurple: toshoBorderPurple ?? this.toshoBorderPurple,
+      rtlockBorderPurple: rtlockBorderPurple ?? this.rtlockBorderPurple,
       platinium: platinium ?? this.platinium,
       gray85: gray85 ?? this.gray85,
     );
@@ -71,8 +71,8 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
       return this;
     }
     return RemoteLockColors(
-      toshoPurple: Color.lerp(toshoPurple, other.toshoPurple, t),
-      darkerToshoPurple: Color.lerp(darkerToshoPurple, other.darkerToshoPurple, t),
+      rtlockPurple: Color.lerp(rtlockPurple, other.rtlockPurple, t),
+      darkerRtlockPurple: Color.lerp(darkerRtlockPurple, other.darkerRtlockPurple, t),
       cadetGray: Color.lerp(cadetGray, other.cadetGray, t),
       antiFlashWhite: Color.lerp(antiFlashWhite, other.antiFlashWhite, t),
       systemRed: Color.lerp(systemRed, other.systemRed, t),
@@ -81,7 +81,7 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
       oxfordBlue: Color.lerp(oxfordBlue, other.oxfordBlue, t),
       white: Color.lerp(white, other.white, t),
       black: Color.lerp(black, other.black, t),
-      toshoBorderPurple: Color.lerp(toshoBorderPurple, other.toshoBorderPurple, t),
+      rtlockBorderPurple: Color.lerp(rtlockBorderPurple, other.rtlockBorderPurple, t),
       platinium: Color.lerp(platinium, other.platinium, t),
       gray85: Color.lerp(gray85, other.gray85, t),
     );
@@ -90,8 +90,8 @@ class RemoteLockColors extends ThemeExtension<RemoteLockColors> {
 // Optional
   @override
   String toString() => '''MyColors(
-toshoPurple : $toshoPurple ,
-darkerToshoPurple : $darkerToshoPurple ,
+rtlockPurple : $rtlockPurple ,
+darkerrtlockPurple : $darkerRtlockPurple ,
 cadetGray : $cadetGray ,
 antiFlashWhite : $antiFlashWhite ,
 systemRed : $systemRed ,
@@ -100,15 +100,15 @@ systemYellow : $systemYellow ,
 oxfordBlue : $oxfordBlue ,
 white : $white ,
 black : $black ,
-toshoBorderPurple : $toshoBorderPurple ,
+rtlockBorderPurple : $rtlockBorderPurple ,
   )
 platinium: $platinium;
 gray85: $gray85''';
 }
 
 const RemoteLockColors lightRemoteLockColors = RemoteLockColors(
-  toshoPurple: Color(0xFF695ED3),
-  darkerToshoPurple: Color(0xFF4437C8),
+  rtlockPurple: Color(0xFF695ED3),
+  darkerRtlockPurple: Color(0xFF4437C8),
   cadetGray: Color(0xFF97A3B7),
   antiFlashWhite: Color(0xFFF5F6F8),
   systemRed: Color(0xFFFF3B30),
@@ -117,7 +117,7 @@ const RemoteLockColors lightRemoteLockColors = RemoteLockColors(
   oxfordBlue: Color(0xFF0E0B28),
   white: Color(0xFFFFFFFF),
   black: Color(0xFF000000),
-  toshoBorderPurple: Color(0xFF6B5EDB),
+  rtlockBorderPurple: Color(0xFF6B5EDB),
   platinium: Color(0xFFD8DCE4),
   gray85: Color.fromARGB(255, 170, 172, 170),
 );
--- lib/design/theme.dart
+++ lib/design/theme.dart
@@ -4,47 +4,45 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:remotelock/design/remotelock_colors.dart';
 import 'package:remotelock/design/remotelock_typo.dart';
 
-
-
 // THEMES
 enum ThemeState { light, dark }
 
 ThemeData getTheme(ThemeState state) {
-  late RemoteLockColors toshoColors;
+  late RemoteLockColors rtlockColors;
   late ThemeData theme;
 
   if (state == ThemeState.dark) {
-    toshoColors = lightRemoteLockColors;
+    rtlockColors = lightRemoteLockColors;
     theme = ThemeData.light();
   } else {
-    toshoColors = lightRemoteLockColors;
+    rtlockColors = lightRemoteLockColors;
     theme = ThemeData.light();
   }
 
   return theme.copyWith(
-    primaryColor: toshoColors.toshoPurple,
+    primaryColor: rtlockColors.rtlockPurple,
 
     // TEXTFIELD
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
-      fillColor: toshoColors.cadetGray!.withAlpha(26),
-      focusColor: toshoColors.cadetGray!.withAlpha(26),
+      fillColor: rtlockColors.cadetGray!.withAlpha(26),
+      focusColor: rtlockColors.cadetGray!.withAlpha(26),
       disabledBorder: const OutlineInputBorder(borderSide: BorderSide.none),
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4.0),
-        borderSide: BorderSide(color: toshoColors.toshoBorderPurple!, width: 1.5),
+        borderSide: BorderSide(color: rtlockColors.rtlockBorderPurple!, width: 1.5),
       ),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4.0),
-        borderSide: BorderSide(color: toshoColors.toshoBorderPurple!, width: 1.5),
+        borderSide: BorderSide(color: rtlockColors.rtlockBorderPurple!, width: 1.5),
       ),
       errorBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4.0),
-        borderSide: BorderSide(color: toshoColors.systemRed!, width: 1.5),
+        borderSide: BorderSide(color: rtlockColors.systemRed!, width: 1.5),
       ),
       focusedErrorBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4.0),
-        borderSide: BorderSide(color: toshoColors.systemRed!, width: 1.5),
+        borderSide: BorderSide(color: rtlockColors.systemRed!, width: 1.5),
       ),
       floatingLabelBehavior: FloatingLabelBehavior.never,
       floatingLabelAlignment: FloatingLabelAlignment.start,
@@ -52,30 +50,15 @@ ThemeData getTheme(ThemeState state) {
 
     //LOADER
     progressIndicatorTheme: ProgressIndicatorThemeData(
-      color: toshoColors.toshoPurple,
+      color: rtlockColors.rtlockPurple,
     ),
-
-    // // CHECKBOX
-    // checkboxTheme: CheckboxThemeData(
-    //   shape: OutlinedBorder(
-    //     side: BorderSide(color: toshoColors.toshoBorderPurple!, width: 1.5),
-    //       borderRadius: BorderRadius.circular(4.0),
-    //       borderSide: BorderSide(color: toshoColors.systemRed!, width: 1.5)),
-    // ),
-
-    // // ELEVATED BUTTON
-    // elevatedButtonTheme: ElevatedButtonThemeData(
-    //   style: ButtonStyle(
-
-    //   )
-    // ),
 
     textTheme: TextTheme(
       displayLarge: TextStyle(
         fontFamily: RemoteLockTypography.familyIntegralCF,
         fontWeight: FontWeight.w700,
         fontSize: 34,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.21,
         letterSpacing: 0.37,
       ),
@@ -85,7 +68,7 @@ ThemeData getTheme(ThemeState state) {
         fontFamily: RemoteLockTypography.familyIntegralCF,
         fontWeight: FontWeight.w700,
         fontSize: 28,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.21,
         letterSpacing: 0.37,
       ),
@@ -93,7 +76,7 @@ ThemeData getTheme(ThemeState state) {
         fontFamily: RemoteLockTypography.familyIntegralCF,
         fontWeight: FontWeight.w700,
         fontSize: 22,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.27,
         letterSpacing: 0.35,
       ),
@@ -101,7 +84,7 @@ ThemeData getTheme(ThemeState state) {
         fontFamily: RemoteLockTypography.familyIntegralCF,
         fontWeight: FontWeight.w700,
         fontSize: 20,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.25,
         letterSpacing: 0.38,
       ),
@@ -111,7 +94,7 @@ ThemeData getTheme(ThemeState state) {
         // fontFamily: RemoteLockTypography.familyMontserrat,
         fontWeight: FontWeight.w400,
         fontSize: 17,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.29,
         letterSpacing: -0.4,
       ),
@@ -120,7 +103,7 @@ ThemeData getTheme(ThemeState state) {
       headlineMedium: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 17,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.29,
         letterSpacing: -0.4,
       ),
@@ -129,7 +112,7 @@ ThemeData getTheme(ThemeState state) {
       headlineSmall: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 16,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.31,
         letterSpacing: -0.32,
       ),
@@ -138,7 +121,7 @@ ThemeData getTheme(ThemeState state) {
       bodyLarge: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 15,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.33,
         letterSpacing: -0.24,
       ),
@@ -147,7 +130,7 @@ ThemeData getTheme(ThemeState state) {
       bodyMedium: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 13,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.38,
         letterSpacing: -0.08,
       ),
@@ -156,7 +139,7 @@ ThemeData getTheme(ThemeState state) {
       labelLarge: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 12,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.33,
       ),
 
@@ -164,260 +147,16 @@ ThemeData getTheme(ThemeState state) {
       labelMedium: GoogleFonts.montserrat(
         fontWeight: FontWeight.w400,
         fontSize: 11,
-        color: toshoColors.oxfordBlue,
+        color: rtlockColors.oxfordBlue,
         height: 1.18,
         letterSpacing: -0.066,
       ),
     ),
     extensions: <ThemeExtension<dynamic>>[
-      toshoColors,
+      rtlockColors,
     ],
   );
 }
-
-// final _theme = ThemeData.light().copyWith(
-//   primaryColor: AppColorsFoundation.pink,
-
-//   //APP BAR
-//   appBarTheme: AppBarTheme(backgroundColor: lightColecColors.white),
-
-//   bottomNavigationBarTheme: BottomNavigationBarThemeData(
-//       type: BottomNavigationBarType.fixed,
-//       backgroundColor: lightColecColors.dark,
-//       selectedIconTheme: IconThemeData(size: 35, color: lightColecColors.pink),
-//       unselectedIconTheme:
-//           IconThemeData(size: 35, color: lightColecColors.white),
-//       selectedItemColor: lightColecColors.pink,
-//       unselectedItemColor: lightColecColors.white),
-//   navigationBarTheme: NavigationBarThemeData(
-//     backgroundColor: lightColecColors.dark,
-//     indicatorColor: Colors.transparent,
-//     iconTheme: MaterialStateProperty.resolveWith<IconThemeData>(
-//         (Set<MaterialState> states) {
-//       if (states.contains(MaterialState.selected)) {
-//         return IconThemeData(size: 35, color: lightColecColors.pink);
-//       }
-//       return IconThemeData(size: 35, color: lightColecColors.white);
-//     }),
-//   ),
-//   textSelectionTheme: TextSelectionThemeData(
-//     selectionColor: lightColecColors.lightGrey,
-//     cursorColor: lightColecColors.lightGrey,
-//     selectionHandleColor: lightColecColors.lightGrey,
-//   ),
-
-//   // TEXTFIELD
-//   inputDecorationTheme: InputDecorationTheme(
-//     filled: true,
-//     fillColor: lightColecColors.white!,
-//     focusColor: lightColecColors.white!,
-//     focusedBorder: OutlineInputBorder(
-//         borderRadius: BorderRadius.circular(8.0),
-//         borderSide:
-//             BorderSide(color: lightColecColors.borderGrey!, width: 1.0)),
-//     enabledBorder: OutlineInputBorder(
-//         borderRadius: BorderRadius.circular(8.0),
-//         borderSide:
-//             BorderSide(color: lightColecColors.borderGrey!, width: 1.0)),
-//     floatingLabelBehavior: FloatingLabelBehavior.never,
-//     floatingLabelAlignment: FloatingLabelAlignment.start,
-//   ),
-//   tabBarTheme: TabBarTheme(
-//       labelPadding: const EdgeInsets.all(8.0),
-//       indicatorColor: lightColecColors.dark!,
-//       indicatorSize: TabBarIndicatorSize.tab),
-//   filledButtonTheme: FilledButtonThemeData(
-//       style: ButtonStyle(
-//     //     textStyle: MaterialStateProperty.resolveWith<TextStyle>((Set<MaterialState> states) {
-//     //   if (states.contains(MaterialState.disabled)) {
-//     //     return ;
-//     //   }
-//     //   return lightColecColors.pink!;
-//     // }),,
-//     foregroundColor:
-//         MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-//       if (states.contains(MaterialState.disabled)) {
-//         return AppColorsFoundation.white.withAlpha(100);
-//       }
-//       return lightColecColors.white!;
-//     }),
-//     backgroundColor:
-//         MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-//       if (states.contains(MaterialState.disabled)) {
-//         return AppColorsFoundation.subltePink;
-//       }
-//       if (states.contains(MaterialState.hovered)) {
-//         return lightColecColors.lightPink!;
-//       }
-//       if (states.contains(MaterialState.pressed)) {
-//         return lightColecColors.darkerPink!;
-//       }
-//       if (states.contains(MaterialState.focused)) {
-//         return lightColecColors.pink!;
-//       }
-//       return lightColecColors.pink!;
-//     }),
-//   )),
-//   outlinedButtonTheme: OutlinedButtonThemeData(
-//       style: ButtonStyle(
-//     shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
-//         (Set<MaterialState> states) {
-//       if (states.contains(MaterialState.focused)) {
-//         return StadiumBorder(
-//             side: BorderSide(color: lightColecColors.pink!, width: 1.0));
-//       }
-//       return const StadiumBorder(side: BorderSide(width: 0.5));
-//     }),
-//     foregroundColor:
-//         MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-//       if (states.contains(MaterialState.disabled)) {
-//         return AppColorsFoundation.white.withAlpha(100);
-//       }
-//       if (states.contains(MaterialState.hovered) ||
-//           states.contains(MaterialState.pressed)) {
-//         return lightColecColors.white!;
-//       }
-//       return lightColecColors.dark!;
-//     }),
-//     backgroundColor:
-//         MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-//       if (states.contains(MaterialState.disabled)) {
-//         return AppColorsFoundation.dark3;
-//       }
-//       if (states.contains(MaterialState.hovered)) {
-//         return lightColecColors.lightGrey!;
-//       }
-//       if (states.contains(MaterialState.pressed)) {
-//         return lightColecColors.dark!;
-//       }
-//       return lightColecColors.white!;
-//     }),
-//   )),
-//   // textButtonTheme: TextButtonThemeData(),
-//   switchTheme: SwitchThemeData(
-//     trackColor:
-//         MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-//       if (states.contains(MaterialState.disabled)) {
-//         return AppColorsFoundation.lightGrey2;
-//       }
-//       return AppColorsFoundation.pink;
-//     }),
-//   ),
-//   // cupertinoOverrideTheme: Cuper,
-//   // primarySwatch: AppColorsFoundation.lightGrey,
-//   textTheme: const TextTheme(
-//     // displayLarge: TextStyle(
-//     //   fontFamily: AppTypographyFoundation.familyHeadings,
-//     //   fontWeight: FontWeight.w700,
-//     //   fontSize: AppTypographyFoundation.fontSizeH1,
-//     //   color: AppColorsFoundation.dark,
-//     // ),
-//     // displayMedium: TextStyle(
-//     //   fontFamily: AppTypographyFoundation.familyHeadings,
-//     //   fontWeight: FontWeight.w900,
-//     //   fontSize: AppTypographyFoundation.fontSizeH2,
-//     //   color: AppColorsFoundation.dark,
-//     // ),
-//     // Figma => Display
-//     displaySmall: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyHeadings,
-//       fontWeight: FontWeight.w300,
-//       fontSize: AppTypographyFoundation.fontSizeDisplaySmall,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Header
-//     headlineLarge: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyHeadings,
-//       fontWeight: FontWeight.w300,
-//       fontSize: AppTypographyFoundation.fontSizeHeadlineLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => N/A
-//     headlineMedium: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyHeadings,
-//       fontWeight: FontWeight.w400,
-//       fontSize: AppTypographyFoundation.fontSizeTitleLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Title 1
-//     headlineSmall: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyHeadings,
-//       fontWeight: FontWeight.w700,
-//       fontSize: AppTypographyFoundation.fontSizeTitleLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Title 2
-//     titleLarge: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyBody,
-//       fontWeight: FontWeight.w700,
-//       fontSize: AppTypographyFoundation.fontSizeTitleMedium,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Headline
-//     titleMedium: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyBody,
-//       fontWeight: FontWeight.w700,
-//       fontSize: AppTypographyFoundation.fontSizeLabelLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Body
-//     // Size : 16
-//     titleSmall: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyBody,
-//       fontWeight: FontWeight.w400,
-//       fontSize: AppTypographyFoundation.fontSizeLabelSmall,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Caption
-//     // Size 14
-//     labelLarge: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyBody,
-//       fontWeight: FontWeight.w400,
-//       fontSize: AppTypographyFoundation.fontSizeBodyLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Extra Large label
-//     // Size 18
-//     labelMedium: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyLabel,
-//       fontWeight: FontWeight.w600,
-//       fontSize: AppTypographyFoundation.fontSizeLabelMedium,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Large label
-//     labelSmall: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyLabel,
-//       fontWeight: FontWeight.w600,
-//       fontSize: AppTypographyFoundation.fontSizeLabelSmall,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Normal label
-//     bodyLarge: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyLabel,
-//       fontWeight: FontWeight.w600,
-//       fontSize: AppTypographyFoundation.fontSizeBodyLarge,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Small label
-//     bodyMedium: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyLabel,
-//       fontWeight: FontWeight.w400,
-//       fontSize: AppTypographyFoundation.fontSizeBodyMedium,
-//       color: AppColorsFoundation.dark,
-//     ),
-//     // Figma => Extra small label
-//     bodySmall: TextStyle(
-//       fontFamily: AppTypographyFoundation.familyLabel,
-//       fontWeight: FontWeight.w400,
-//       fontSize: AppTypographyFoundation.fontSizeBodySmall,
-//       color: AppColorsFoundation.dark,
-//     ),
-//   ),
-//   extensions: const <ThemeExtension<dynamic>>[
-//     lightColecColors,
-//     colecSpacing,
-//   ],
-// );
 
 // THEME PROVIDERS
 final themeProvider = Provider((ref) {
